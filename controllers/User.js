1354b6334f136e9cfdcfb2869559448f8c0fb912
iliaspapam <148943803+iliaspapam@users.noreply.github.com>
2024-12-10 15:37:02 +0200

    Update User.js

--- controllers/User.js
+++ controllers/User.js
@@ -82,3 +82,63 @@ module.exports.removeUserFile = function removeUserFile(req, res, next, userid, 
       }
     });
 };
+
+module.exports.searchCourseDB = function searchCourseDB (req, res, next, keyword) {
+  User.searchCourseDB(keyword)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.uploadNewFile = function uploadNewFile (req, res, next, body, userid) {
+  User.uploadNewFile(body, userid)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.userFavouritesFiles = function userFavouritesFiles (req, res, next, userid) {
+  User.userFavouritesFiles(userid)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.userFileEditing = function userFileEditing (req, res, next, body, userid, fileid) {
+  User.userFileEditing(body, userid, fileid)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.viewReview = function viewReview (req, res, next, courseid) {
+  User.viewReview(courseid)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.writeReview = function writeReview (req, res, next, body, courseid) {
+  User.writeReview(body, courseid)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
