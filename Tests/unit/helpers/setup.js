d223922d1553127a0a7ceefd0189af3b87817389
iliaspapam <iliaspapamichail@gmail.com>
2024-12-30 16:48:36 +0200

    Add comments

--- Tests/unit/helpers/setup.js
+++ Tests/unit/helpers/setup.js
@@ -2,6 +2,7 @@ const http = require('http');
 const got = require('got');
 const app = require('../../../index.js');
 
+//Setup a local server to perform the unit-testing
 async function setup(t) {
     t.context.server = http.createServer(app);
     const server = t.context.server.listen();
@@ -12,8 +13,10 @@ async function setup(t) {
     });
 }
 
+// Teardown the server and release the port after tests are finished
 function teardown(t) {
     t.context.server.close();
 }
 
+// Export the setup and teardown functions for use in tests
 module.exports = { setup, teardown };
